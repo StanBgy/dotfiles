598ed14a43fcfeabe40f9946f1e23900a585d5ed
stan <stan@stans-MacBook-Pro.local>
2026-04-22 18:33:00 +0200

    pyright updated for no stupid ass diagnostics

--- nvim/lsp/pyright.lua
+++ nvim/lsp/pyright.lua
@@ -10,6 +10,16 @@ return {
     'pyrightconfig.json',
     '.git',
   },
+  on_init = function(client)
+    client.server_capabilities.diagnosticProvider = false
+    client.server_capabilities.publishDiagnostics = false
+  end,
+  -- We also manually clear the capabilities just to be safe
+  capabilities = (function()
+    local caps = vim.lsp.protocol.make_client_capabilities()
+    caps.textDocument.publishDiagnostics = false
+    return caps
+  end)(),
   settings = {
     python = {
       analysis = {
@@ -17,6 +27,12 @@ return {
         autoSearchPaths = true,
         useLibraryCodeForTypes = true,
         diagnosticMode = 'openFilesOnly',
+        diagnosticSeverityOverrides = {
+          reportInvalidTypeForm = "none",
+          reportMissingImports = "none",
+          reportUndefinedVariable = "none",
+          reportGeneralTypeIssues = "none",
+        },
       },
     },
   },
